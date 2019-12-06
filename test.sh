1f6005183ccf1295936cf49fc22792f81bdb33fd
Boaz Rosenan <brosenan@brosenan-macbookpro.roam.corp.google.com>
2019-12-06 21:16:25 +0200

    .md file renaming

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 this_dir=$(dirname $0)
 
-md_files="type-system.md unions.md"
+md_files="simple-expressions.md simple-functions.md unions.md"
 
 $this_dir/swipl -f $this_dir/neutrino.pl -t run_tests
 
